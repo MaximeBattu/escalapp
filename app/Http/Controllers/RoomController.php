80a69247ca7221dc45ac637248c3132f6fd8f30c
Yedenn <ianis.olivier.pacaud@gmail.com>
2020-01-15 10:45:11 +0100

    fix sql error statement when violating constraints

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
 use App\FinishedRoute;
 use App\Room;
 use App\User;
@@ -93,12 +94,16 @@ class RoomController extends Controller
         $email = $request->input('emailRoom');
         $address = $request->input('addressRoom');
 
-        Room::create([
-            'name_room' => htmlspecialchars($name),
-            'email' => htmlspecialchars($email),
-            'address_room' => htmlspecialchars($address),
-            'updated_at' => null
-        ]);
+        try {
+            Room::create([
+                'name_room' => htmlspecialchars($name),
+                'email' => htmlspecialchars($email),
+                'address_room' => htmlspecialchars($address),
+                'updated_at' => null
+            ]);
+        } catch (QueryException $ex) {
+            return redirect()->back()->with('add_failure', 'La salle existe déjà');
+        }
 
         if ($request->submit == 'Ajouter')
             return redirect('/admin/gestion-salle')->with('add-success', 'Successful ! You have added a new room !');
