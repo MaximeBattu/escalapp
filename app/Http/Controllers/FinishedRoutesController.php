27e5510896d0fa9b433ba4690e76917fa2e293ba
iolivierp <ianis.olivier.pacaud@gmail.com>
2020-02-13 11:04:40 +0100

    fix division by zero issue when someone uncheck a route +no use of SQL trigger anymore

--- app/Http/Controllers/FinishedRoutesController.php
+++ app/Http/Controllers/FinishedRoutesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Http\Controllers;
+namespace App\Http\Controllers;
 
 use Doctrine\DBAL\Query\QueryException;
 use Illuminate\Support\Facades\Auth;
@@ -15,22 +15,39 @@ class FinishedRoutesController extends Controller
         $route = Route::find($id);
         $sector = Sector::find($route->id_sector);
 
-        try {
-            FinishedRoute::create([
-                'id_route' => $id,
-                'id_user' => Auth::user()->id,
-                'id_sector' => $sector->id_sector,
-                'type_route' => $sector->climbing_type
+        $updt_users = $route->nb_user_done+1;
+        $updt_score = 1000/$updt_users >= 1 ? 1000/$updt_users : 1; 
+
+        FinishedRoute::create([
+            'id_route' => $id,
+            'id_user' => Auth::user()->id,
+            'id_sector' => $sector->id_sector,
+            'type_route' => $sector->climbing_type
+        ]);
+
+        Route::where('id_route',$id)
+            ->update([
+                'nb_user_done'=>$updt_users,
+                'score_route'=>$updt_score
             ]);
-        } catch (QueryException $e) {
-            return redirect()->back();
-        }
+
         return redirect()->back();
     }
 
     public function deleteValidatedRoute(int $id)
     {
         FinishedRoute::where(['id_route' => $id, 'id_user' => Auth::user()->id])->delete();
+
+        $route = Route::find($id);
+
+        $updt_users = $route->nb_user_done-1;
+        $updt_score = $updt_users >= 1 ? 1000/$updt_users : 1000;
+
+        Route::where('id_route',$id)
+            ->update([
+                'nb_user_done'=>$updt_users,
+                'score_route'=>$updt_score
+            ]);
 
         return redirect()->back();
     }
